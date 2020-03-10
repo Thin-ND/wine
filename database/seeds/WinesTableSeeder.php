b8f19ecfece6eeab8f59995d5b30b521a3feee90
thinnd <thin-nd@gmail.com>
2020-03-10 13:28:39 +0700

    bug

---
+++ database/seeds/WinesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class WinesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Wine::create([
+            'name' => 'Classic Chianti',
+            'description' => 'A medium-bodied wine characterized by a marvelous freshness with a lingering, fruity finish',
+            'color' => 'red',
+            'grape_variety' => 'Sangiovese',
+            'country' => 'Italy'
+        ]);
+    }
+}
