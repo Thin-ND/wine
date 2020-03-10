78edb510cd027309c363d92872ba247391ef34e2
Van Dung <phamvandung1508@gmail.com>
2020-03-10 14:10:54 +0700

    fix seeder

--- database/seeds/WinesTableSeeder.php
+++ database/seeds/WinesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Wine;
 
 class WinesTableSeeder extends Seeder
 {
@@ -11,7 +12,6 @@ class WinesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
         Wine::create([
             'name' => 'Classic Chianti',
             'description' => 'A medium-bodied wine characterized by a marvelous freshness with a lingering, fruity finish',
